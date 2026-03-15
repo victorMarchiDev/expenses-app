f5a280e4d2b44f604a11b3467dbb5dff6c82039c
Victor Marchi <101611816+victorMarchiDev@users.noreply.github.com>
2026-03-14 21:39:31 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(ExpensesApp());
+}
+
+class ExpensesApp extends StatelessWidget {
+  const ExpensesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: MyHomePage());
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Despesas Pessoais')),
+      body: Center(child: Text('Versao Inicial')),
+    );
+  }
+}
